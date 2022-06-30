6fdadea548c50c1d3078f64010a8e3ddae5e657c
Jesse Liberty <jesse.liberty@cnhind.com>
2022-06-30 11:57:32 -0400

    Update to MAUI syntax and use ViewModel to supply list of zips.

---
+++ LearningMaui-Part7/LearningMaui-Part7/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using LearningMaui_Part7.Model;
+
+namespace LearningMaui_Part7.ViewModel
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            CreateList();
+        }
+
+        private List<Result> _resultList;
+        public List<Result> ResultList
+        {
+            get => _resultList;
+            set => SetProperty(ref _resultList, value);
+        }
+
+        public void CreateList()
+        {
+            ResultList = new List<Result>();
+
+            Result result = new Result
+            {
+                image = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Seal_of_New_York_%28state%29.svg/150px-Seal_of_New_York_%28state%29.svg.png",
+                state = "NY",
+                zip = "07748"
+            };
+            ResultList.Add(result);
+
+            ResultList.Add(new Result
+            {
+                image = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Flag_of_Vermont.svg/188px-Flag_of_Vermont.svg.png",
+                state = "VT",
+                zip = "05143"
+            });
+
+            ResultList.Add(new Result
+            {
+                image = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Flag_of_Connecticut.svg/188px-Flag_of_Connecticut.svg.png",
+                 state = "CT",
+                 zip = "06457" 
+            });
+
+            ResultList.Add(new Result
+            {
+                image = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Flag_of_Connecticut.svg/188px-Flag_of_Connecticut.svg.png",
+                state = "CT",
+                zip = "06457"
+            });
+
+
+        }
+    }
+}
---
+++ LearningMaui-Part7/LearningMaui-Part7/ViewModel/ViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LearningMaui_Part7.ViewModel;
+
+public class ViewModelBase : INotifyPropertyChanged
+{
+    protected ViewModelBase() { }
+
+    protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (Equals(backingStore, value)) return false;
+
+        backingStore = value;
+
+        OnPropertyChanged(propertyName);
+
+        return true;
+    }
+
+
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        var handler = PropertyChanged;
+        if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
